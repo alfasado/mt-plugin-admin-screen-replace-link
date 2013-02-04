213080bcd5b8ebc93fbbd0d9740a7f3477c67a75
Alfasado Inc <webmaster@alfasado.jp>
2013-02-04 19:17:36 +0900

    Change directry name.

---
+++ plugins/AdminScreenReplaceLink/lib/AdminScreenReplaceLink/L10N/ja.pm
@@ -0,0 +1,11 @@
+package AdminScreenReplaceLink::L10N::ja;
+use strict;
+use base 'AdminScreenReplaceLink::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'It replace link in admin screen.' => '管理画面のリンクURLを置換します。',
+    'Replace' => '置換',
+ );
+
+1;
